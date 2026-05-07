309fcdaf4ffe62373c01571e5d7a4894c73943fe
StrawBerryGelato <strawberrygelatos@gmail.com>
2026-05-07 00:49:15 -0400

    Update ToDoList.jsx

--- pomodoro-timer/src/ToDoList.jsx
+++ pomodoro-timer/src/ToDoList.jsx
@@ -22,13 +22,13 @@ function ToDoList() {
     {/* I was lazy with the CSS and just slapped on buttons and change the font color and size manually instead of doing it in the CSS file*/}
       <h2 style={{ color: 'white', fontSize: '30px' }}>To-Do List</h2>
 
-      <input ref={inputRef} placeholder="Add a task..." />
+      <input ref={inputRef} placeholder="Add a task..." style={{padding: "10px",border: "0px",borderRadius: "12px",fontSize: "16px",}}/>
       <button onClick={addItem}>Add</button>
 
       <ul>
         {items.map(item => (
           <li key={item.id}>
-            <span style={{ color: 'white', fontSize: '30px' }}>
+            <span style={{color: 'white', fontSize: '30px',backgroundColor: 'rgba(255,255,255,0.2)',padding: '8px 100px', borderRadius: '12px', }}>
               {item.text}
             </span >
             <button onClick={() => removeItem(item.id)}>✕</button>
